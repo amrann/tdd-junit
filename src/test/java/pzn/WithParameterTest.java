bf7c69019e2e2ed5efa24f5659b06bf850db25bc
meran <amr.bin.an@gmail.com>
2023-07-01 13:22:20 +0700

    testing unit with parameter using @methodsource

--- src/test/java/pzn/WithParameterTest.java
+++ src/test/java/pzn/WithParameterTest.java
@@ -3,10 +3,17 @@ package pzn;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class WithParameterTest {
   private Calculator calculator = new Calculator();
+  public static List<Integer> parameterSource() {
+    return Arrays.asList(3, 123, 99, 10);
+  }
 
   @DisplayName("Percobaan Test")
   @ParameterizedTest(name = "{displayName} dengan parameter {0}")
@@ -16,6 +23,20 @@ public class WithParameterTest {
     int result = calculator.add(value, value);
     Assertions.assertEquals(expected, result);
   }
+
+  @DisplayName("Percobaan Test")
+  @ParameterizedTest(name = "{displayName} dengan parameter {0}")
+  @MethodSource({"parameterSource"})
+  void usingParameterMethodSource(int value) {
+    int expected = value + value;
+    int result = calculator.add(value, value);
+    Assertions.assertEquals(expected, result);
+  }
 }
+
+
+
+
+
 
 
