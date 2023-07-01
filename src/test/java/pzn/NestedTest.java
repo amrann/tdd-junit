7a7d20f3f49f5eae0059ed05e376faf550df02f0
meran <amr.bin.an@gmail.com>
2023-07-01 10:26:32 +0700

    nested testing

---
+++ src/test/java/pzn/NestedTest.java
@@ -0,0 +1,31 @@
+package pzn;
+
+import org.junit.jupiter.api.*;
+
+import java.util.LinkedList;
+import java.util.Queue;
+@DisplayName("A queue")
+public class NestedTest {
+  private Queue<String> queue;
+  @Nested
+  @DisplayName("when new")
+  public class WhenNew {
+    @BeforeEach
+    void setUp() {
+      queue = new LinkedList<>();
+    }
+    @Test
+    @DisplayName("when offer, size must be 1")
+    void offerNewData() {
+      queue.offer("Amran");
+      Assertions.assertEquals(1, queue.size());
+    }
+    @Test
+    @DisplayName("when offer 2 data, size must be 2")
+    void offerMoreData() {
+      queue.offer("Amran");
+      queue.offer("Maruusy");
+      Assertions.assertEquals(2, queue.size());
+    }
+  }
+}
